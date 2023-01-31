855b6f2b7c01b35bd74d8494f6432f84d3e29f02
micaelm <micaelmdossantos@gmail.com>
2023-01-31 19:05:13 -0300

    corrigindo static files (visual)

--- Django1/settings.py
+++ Django1/settings.py
@@ -117,8 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'virtual-production.up.railway.app/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
